1ce66f270c8d55779c7ebb24eead405c4a845226
Paul De Ulloa <pauld143@gmail.com>
2023-08-30 16:47:48 -0700

    first prop created

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,9 +1,12 @@
 const PersonCard = (props) => {
+
+    const { firstName, lastName, age, hairColor } = props;
+
     return(
         <div>
-            <h1 style={{color:"red"}}>Instructor Bob</h1>
-            <p>Age: 32</p>
-            <p>Hair Color: purple</p>
+            <h1 style={{color:"green"}}>{lastName}, {firstName}</h1>
+            <p>Age: {age}</p>
+            <p>Hair Color: {hairColor}</p>
         </div>
     );
 }

c8d3c94f8391cdf7a03170e19d32a5d1547a2d0c
Paul De Ulloa <pauld143@gmail.com>
2023-08-30 17:13:41 -0700

    Birthday button added for each PersonCard.

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -2,11 +2,16 @@ const PersonCard = (props) => {
 
     const { firstName, lastName, age, hairColor } = props;
 
+    const alertMSG = () => {
+        alert("Happy Birthday!")
+    }
+
     return(
         <div>
             <h1 style={{color:"green"}}>{lastName}, {firstName}</h1>
             <p>Age: {age}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={ () => alert("Birthday Button has been clicked")}>Birthday Button for {firstName} {lastName}</button>
         </div>
     );
 }
